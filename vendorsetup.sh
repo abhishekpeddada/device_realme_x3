69bff04df0ab53649002f30eed09a6b56b0bf274
akmacc <arumugamguna7058@gmail.com>
2023-02-23 17:24:33 +0000

    change kernel

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,10 +2,10 @@
 git clone https://github.com/akmacc/vendor_realme_x3.git -b 13 vendor/realme/x3
 
 # Kernel Tree
-git clone https://github.com/akmacc/kernel_realme_x3.git -b msm-4.14 kernel/realme/x3
+git clone https://github.com/akmacc/android_kernel_realme_x3.git -b upstream  kernel/realme/x3
 
 # Clang
-git clone --depth=1 https://gitlab.com/varunhardgamer/trb_clang.git -b 16 prebuilts/clang/host/linux-x86/trb_clang
+git clone --depth=1 https://gitlab.com/varunhardgamer/trb_clang.git -b 17 prebuilts/clang/host/linux-x86/trb_clang
 
 # Other
 git clone https://github.com/Realme-X3-Series/hardware_oplus.git hardware/oplus
