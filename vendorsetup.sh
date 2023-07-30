89436df6e67e2996a95e0b673e8a74a06395132f
abhishekpeddada <abhishekpeddada@gmail.com>
2023-07-30 16:36:47 +0530

    x3: add vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/akmacc/kernel_realme_x3-2 kernel/realme/x3
+git clone https://github.com/akmacc/vendor_realme_x3 vendor/realme/x3
+git clone https://github.com/akmacc/hardware_oplus hardware/oplus
+git clone https://gitlab.com/PixelOS-Devices/playgroundtc.git -b 16 prebuilts/clang/host/linux-x86/clang-playground
